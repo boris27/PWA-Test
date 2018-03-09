2c2071679e0fba74504ce6855c9334eda1166a5d
boris27 <btolstyakov@gmail.com>
2018-03-10 00:23:42 +0200

    Service Worker test

--- sw.js
+++ sw.js
@@ -21,6 +21,13 @@ self.addEventListener('activate', (e)=> {
           }
       } )))
     );
-    console.log(self);
     return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e)=> {
+    console.log(e);
+   e.respondWith(
+       caches.match(e.request)
+           .then( (response)=> response || fetch(e.request) )
+   )
 });
